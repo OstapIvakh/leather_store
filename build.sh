8a8a4af27678e472fbc3404c51e5af526f77df72
OstapIvakh <o.ivakh95@gmail.com>
2026-04-27 15:51:32 +0200

    Add gunicorn and fix static folder

--- build.sh
+++ build.sh
@@ -2,8 +2,15 @@
 # exit on error
 set -o errexit
 
+# Оновлюємо pip і встановлюємо залежності
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 
+# Створюємо папку static, якщо її немає, щоб Django не сварився
+mkdir -p static
+
+# Збираємо статичні файли
 python manage.py collectstatic --no-input
+
+# Запускаємо міграції
 python manage.py migrate
