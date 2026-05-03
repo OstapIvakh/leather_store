dee178dd461c1a74f0a92c2eec77f1b3a8e72ca0
OstapIvakh <o.ivakh95@gmail.com>
2026-05-03 13:18:45 +0200

    Refactor build script to remove superuser creation logic, add pytest-playwright dependency, new tests for catalog views.

--- build.sh
+++ build.sh
@@ -14,16 +14,3 @@ python manage.py collectstatic --no-input
 
 # Запускаємо міграції
 python manage.py migrate
-
-python manage.py shell -c "
-from django.contrib.auth import get_user_model
-User = get_user_model()
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'ostap@example.com', 'very_complicated_password_123')
-else:
-    u = User.objects.get(username='admin')
-    u.set_password('very_complicated_password_123')
-    u.save()
-    print('Password reset')
-print('Done')
-"
