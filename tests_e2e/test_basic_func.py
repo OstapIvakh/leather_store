0afc99a08b6df92e1dbf56ca6d9bd508b9697333
OstapIvakh <o.ivakh95@gmail.com>
2026-05-06 23:53:53 +0200

    Refactor E2E tests to use dynamic base URL, improve test descriptions, and enhance visibility checks for page elements.

--- tests_e2e/test_basic_func.py
+++ tests_e2e/test_basic_func.py
@@ -1,30 +1,39 @@
 import re
 from playwright.sync_api import Page, expect
 
-BASE_URL = "http://localhost:8000/"
 
 
-def test_homepage_accessible(page: Page):
-    page.goto(BASE_URL)
+def test_shop_page_accessible(page: Page, base_url: str):
+    """Test that the shop page is accessible."""
+    page.goto(base_url)
+    page.locator("#navbarNav").get_by_role("link", name="Shop").click()
+    expect(page).to_have_url(re.compile(".*/catalog/"))
+
+
+def test_homepage_accessible(page: Page, base_url: str):
+    """Test that the homepage is accessible."""
+    page.goto(base_url)
     expect(page).to_have_title("KOROL LEATHER WORKSHOP")
-    expect(page).to_have_url(BASE_URL)
+    expect(page).to_have_url(base_url)
 
 
-def test_category_page_accessible(page: Page):
-    page.goto(f"{BASE_URL}/catalog/wallets/")
+def test_category_page_accessible(page: Page, base_url: str):
+    """Test that the category page is accessible."""
+    page.goto(f"{base_url}catalog/wallets/")
     expect(page).to_have_url(re.compile(".*/wallets/"))
-    header = page.get_by_role("heading", name="Wallets")
-    expect(header).to_be_visible()
+    expect(page.get_by_role("heading", name="Wallets")).to_be_visible()
 
 
-def test_product_detail_page_accessible(page: Page):
-    page.goto(f"{BASE_URL}/product/1/classic-bifold-wallet/")
-    product_header = page.get_by_role("heading", name="Classic bifold wallet")
+def test_product_detail_page_accessible(page: Page, base_url: str):
+    """Test that the product detail page is accessible."""
+    page.goto(f"{base_url}catalog/")
+    page.locator(".card a").first.click()
+    product_header = page.locator("h1")
     expect(product_header).to_be_visible()
 
 
-def test_cart_page_accessible(page: Page):
-    page.goto(f"{BASE_URL}/cart/")
+def test_cart_page_accessible(page: Page, base_url: str):
+    """Test that the cart page is accessible."""
+    page.goto(f"{base_url}cart/")
     expect(page).to_have_url(re.compile(".*/cart/"))
-    header = page.locator("h2")
-    expect(header).to_have_text("Your Shopping Cart")
+    expect(page.get_by_role("heading", name="Your Shopping Cart")).to_be_visible()
